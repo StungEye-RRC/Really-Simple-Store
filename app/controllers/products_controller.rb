4af8957b80f30b7ba26d7ffffb21a93665732e84
Kyle Geske <stungeye@gmail.com>
2014-11-12 10:38:11 -0600

    Added a hardcoded username and password for the products and categories controllers.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :set_categories, only: [:new, :edit]
+  before_action :require_admin_user
 
   # GET /products
   # GET /products.json
